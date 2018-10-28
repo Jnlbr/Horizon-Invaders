6b8a19366cac9793e25c47a37b356ddcc2c26e8b
Jean Lambert <jeeaan1702@gmail.com>
2018-10-28 11:22:05 -0400

    new folder org

--- scripts/extra.js
+++
@@ -1,9 +0,0 @@
-function play(track_id) {
-    var audio = document.getElementById(track_id);
-    audio.play();
-}
-
-function pause(track_id) {
-    var audio = document.getElementById(track_id);
-    audio.pause();
-}
